6f538a7f0e11c1885a8dbec2c67bb2f3d65e53da
Srini <srinivas.mtec@gmail.com>
2020-10-10 18:53:04 +0100

    rev4

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -5,7 +5,6 @@ public class MainTest {
         System.out.println("Main commit");
         System.out.println("first commit");
         System.out.println("Third commit");
-        System.out.println("fourth commit");
 
     }
 }
